0b376a7f5e285f96e9e839feed67086a0ee356a0
PRASANTH G <prasanth287471@gmail.com>
2021-03-23 16:44:25 +0000

    first commit

--- Main.java
+++ Main.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
 class Main {
-  public static void main(String[] args) {
-    
+  public static void main(String[] args) 
+	{
+    Scanner scanner=new Scanner (System.in);
+		byte age = scanner.nextByte();
+System.out.println("you are "+ age);
+
   }
 }
